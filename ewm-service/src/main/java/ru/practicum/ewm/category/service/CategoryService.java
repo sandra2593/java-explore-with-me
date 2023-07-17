e345d664326736b2d1f1c526bf3df9a371946325
azaytseva <azaytseva@ozon.ru>
2023-07-17 22:16:50 +0100

    checkstyle

--- ewm-service/src/main/java/ru/practicum/ewm/category/service/CategoryService.java
+++ ewm-service/src/main/java/ru/practicum/ewm/category/service/CategoryService.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.category.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
